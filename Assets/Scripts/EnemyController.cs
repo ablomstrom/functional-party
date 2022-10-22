939882bc1c959b596b3127247b00b3d787c996fc
blomstranton <blomstra@arcada.fi>
2022-10-23 00:58:04 +0300

    Test level

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+
+    private GameObject levelManager;
+    private GameObject[] navigateWaypoints;
+    private Transform nextWaypoint;
+
+    private float moveSpeed = 0.05f;
+    private float nextWaypointDistane = 1f;
+    private int currentWaypointIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelManager = GameObject.FindGameObjectWithTag("LM");
+        navigateWaypoints = levelManager.GetComponent<LevelController>().navigateWaypoints;
+        nextWaypoint = navigateWaypoints[currentWaypointIndex].transform;
+        this.transform.position = levelManager.GetComponent<LevelController>().spawnPoint.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveEnemy();
+    }
+
+
+    void MoveEnemy()
+    {
+        if (Vector3.Distance(this.transform.position, nextWaypoint.position) < nextWaypointDistane)
+        {
+            currentWaypointIndex++;
+            nextWaypoint = navigateWaypoints[currentWaypointIndex].transform;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, nextWaypoint.position, moveSpeed);
+    }
+}
--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
+    
+    public GameObject spawnPoint;
+
+    public GameObject[] navigateWaypoints;
 
     [SerializeField]
-    private Vector3[] navigateWaypoints;
+    private GameObject[] enemies;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnPoint = GameObject.FindGameObjectWithTag("SpawnPoint");
+        Instantiate(enemies[0], spawnPoint.transform.position, Quaternion.identity);
     }   
 
     // Update is called once per frame
